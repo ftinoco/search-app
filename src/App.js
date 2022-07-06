724438c80acaa2f530fcaaff58d459319d11346d
Fernando Tinoco <Fernando.Tinoco@stepstone.com>
2022-07-06 11:19:16 -0600

    Consuming country api endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const request_headers = new Headers();
+    const api_key = 'beMYso6KrgPLE1GhKBthDv6AycEKOdsqyqn1XHk6';
+    request_headers.append('Authorization', `Bearer ${api_key}`);
+    request_headers.append('Content-Type', 'application/json');
+
+    fetch('https://countryapi.io/api/all', {
+      method: 'GET',
+      headers: request_headers
+    })
+    .then((req) => req.json())
+    .then((res) => {
+      setLoaded(true);
+      setItems(res);
+    },
+    (error) => {
+      setLoaded(true);
+      setError(error);
+    });
+  }, []);
+
+  //console.log(items);
+  const data = Object.values(items);
+
+  if (error) {
+    return <>{error.message}</>
+  } else if(!loaded) {
+    return <>loading...</>
+  }else {
+    return (
+      <div className="wrapper">
+        <ul className="card-grid">
+        {data.map((item) => {
+          <li key={item.alpha3Code}>
+            <article className="card">
+              <div className="card-image">
+                <img src={item.flag.large} alt={item.name} />
+              </div>
+              <div className="card-content">
+                <h2 className="card-name">{item.name}</h2>
+              </div>
+            </article>
+          </li>
+        })}
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
