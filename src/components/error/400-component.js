e20683372eb7388fb6a44bdc67fbffd552e64e88
Fernando Tinoco <Fernando.Tinoco@stepstone.com>
2022-07-13 15:50:43 -0600

    Adding error component with styles rules

---
+++ src/components/error/400-component.js
@@ -0,0 +1,14 @@
+export const BadRequest = () =>{
+    return (
+        <>
+            <div className="full-screen">
+                <div className='container'>
+                    <span className="error-num">4</span>
+                    <span className="error-num">0</span>
+                    <span className="error-num">0</span>
+                    <p className="sub-text">Request was invalid.</p>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/error/403-component.js
@@ -0,0 +1,14 @@
+export const Forbidden = () => {
+    return (
+        <> 
+          <div className="full-screen">
+                <div className='container'>
+                    <span className="error-num">4</span>
+                    <span className="error-num">0</span>
+                    <span className="error-num">3</span>
+                    <p className="sub-text">The API key is invalid.</p>
+                </div>
+            </div>
+        </>
+    );
+}
